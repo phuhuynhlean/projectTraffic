bc9f0ec70ad7445b76122a4195eb335678422168
AlbusNotThuan <107522443+AlbusNotThuan@users.noreply.github.com>
2022-12-12 21:46:39 +0700

    Update analyzer.py
    Tui nghĩ cách tìm median này gọn hơn nè

--- modules/analyzer.py
+++ modules/analyzer.py
@@ -10,13 +10,15 @@ def data_analyzing(trafficData):
     stdVal = str(floor(std(trafficData[i])))
     maxVal = trafficData[i].index(max(trafficData[i])) + 8
     maxVal = str(maxVal) +":00 - " + str(maxVal+1)+":00"
-    temp = 0; hour = 8
-    for j in trafficData[i]:
-      temp += j
-      if(2*temp >= int(total)):
-        break
-      hour +=1
-    medianVal = str(hour) +":00 - " + str(hour+1)+":00"
+    medianVal = trafficData[i].index(median(trafficData[i])) + 8
+    medianVal = str(medianVal) +":00 - " + str(medianVal+1)+":00"
+    # temp = 0; hour = 8
+    # for j in trafficData[i]:
+    #   temp += j
+    #   if(2*temp >= int(total)):
+    #     break
+    #   hour +=1
+    # medianVal = str(hour) +":00 - " + str(hour+1)+":00"
     outputStr += name[i] + ": " + total +" detected\n  Mean: " + meanVal + "\n  Std deviation: "+stdVal
     outputStr += "\n  Median:" + medianVal + "\n  Rush hour: "+maxVal+"\n\n"
   return outputStr
